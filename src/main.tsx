9b54f7ce211c3ac87ee584db160b609c31bac4d6
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-06-16 15:11:06 +0530

    added preloader

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@
 // Import necessary modules and components
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Preloader from './components/Preloader/Preloader'
 import "./index.css";
 import store from "./app/store";
 import { Provider } from "react-redux";
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Preloader/>
     </Provider>
   </React.StrictMode>
 );
